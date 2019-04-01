8c82a37331c4eef39351021dcdd6ad4fa49a2127
rrosztoczy <rrosztoczy@gmail.com>
2019-04-01 15:20:49 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  for (driver of drivers) {
+  console.log(driver.name)
+  }
+}
+
+function logDriversByHometown(drivers, location) {
+  for (driver of drivers) {
+    if (driver.hometown === location) {
+    console.log(driver.name)
+    }
+  }
+}
+
+function driversByRevenue(drivers) {
+  const newDrivers = [...drivers]
+  const numberSorter = function (driver1, driver2) {
+    return driver1.revenue - driver2.revenue;
+  };
+   
+  return newDrivers.sort(numberSorter);
+}
+
+function driversByName(drivers) {
+  const newDrivers = [...drivers]
+  const nameSorter = function (driver1, driver2) {
+    return driver1.name.localeCompare(driver2.name);
+  };
+   
+  return newDrivers.sort(nameSorter);
+}
+
+function totalRevenue(drivers) {
+  const summer = function (agg, driver) {
+    console.log(agg + driver.revenue)
+    return agg + driver.revenue;
+  };
+   
+  return drivers.reduce(summer, 0);
+}
+
+function averageRevenue(drivers) {
+  const summer = function (agg, driver) {
+    return agg + driver.revenue;
+  };
+   
+  return drivers.reduce(summer, 0)/drivers.length;
+}
+
